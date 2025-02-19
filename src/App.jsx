ebfb27ca83139f751239a37fe78891ae235c07c0
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-02-18 23:15:03 -0500

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, Routes} from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Fruits from './components/Fruits/Fruits'
 import Footer from './components/Footer/Footer'
+import About from './components/About/about'
+import Contact from './components/Contact/Contact'
 function App() {
   return (
     <>
     <NavBar />
-    <Fruits />
+    <Routes>
+      <Route path='/' element={<Fruits />} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
     <Footer />
     </>
   )
