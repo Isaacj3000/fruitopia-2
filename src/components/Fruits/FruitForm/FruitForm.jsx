327d51fa2b7f52ae812e3461a966b342015a3751
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-02-18 23:25:57 -0500

    feat: form

--- src/components/Fruits/FruitForm/FruitForm.jsx
+++ src/components/Fruits/FruitForm/FruitForm.jsx
@@ -1,34 +1,31 @@
-import React, { useState }from "react";
-import "./FruitForm.css";
-function FruitForm({addFruit}) {
+import React, { useState } from 'react'
+
+function FruitForm({ addFruit }) {
     const [name, setName] = useState("");
-    const [emoji, setEmoji] = useState("");
+    const [emoji, setEmoji] = useState("")
     const handleSubmit = (e) => {
         e.preventDefault();
         addFruit(name, emoji);
         setName("");
-        setEmoji("");
-
+        setEmoji("")
     }
-
     return (
         <form onSubmit={handleSubmit}>
-            <input 
-            type="text" 
-            value={name} 
-            placeholder="Enter fruit name"
-            onChange={(e) => setName(e.target.value)}
-
-             />
-             <input 
-             type="text" 
-             value={emoji}
-             placeholder="Enter fruit emoji (e.g. 🍎)"
-             onChange={(e) => setEmoji(e.target.value)} 
-
-             />
-             <button type="submit">Add Fruit</button>
+            <input
+                type="text"
+                value={name}
+                placeholder='Enter fruit name'
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input
+                type="text"
+                value={emoji}
+                placeholder='Enter fruit emoji (e.g. 🍎)'
+                onChange={(e) => setEmoji(e.target.value)}
+            />
+            <button type="submit">Add Fruit</button>
         </form>
     )
 }
-    export default FruitForm;
+
+export default FruitForm
