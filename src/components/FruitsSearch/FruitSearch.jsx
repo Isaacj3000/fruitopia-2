736f0350bc43a46e5b82636a7a02cc10dde27b5a
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-02-18 23:26:32 -0500

    feat: add search and details

--- src/components/FruitsSearch/FruitSearch.jsx
+++ src/components/FruitsSearch/FruitSearch.jsx
@@ -1,38 +1,85 @@
-import React from 'react'
-import { show } from '../../services/fruitService';
+import React, { useState, useEffect } from 'react'
+import "./Fruits.css"
+import FruitForm from './FruitForm/FruitForm';
+import FruitList from './FruitList/FruitList';
+import { getAllFruits } from '../../services/fruitService';
+import FruitSearch from './FruitsSearch/FruitSearch';
+function Fruits() {
 
-function FruitSearch() {
-    const [fruit, setFruit] = useState('');
-    const [fruitData, setFruitData] = useState('');
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!fruit.trim()) return
-         try {
-            const data = await show(fruit);
-            setFruit(data);
-        } catch (error) {
-            console.error("Error fetching Fruit data:", error);
+    const [fruits, setFruits] = useState([])
+    const [collectedFruits, setCollectedFruits] = useState([]);
+    useEffect(() => {
+        const fetchFruits = async () => {
+            const allFruits = await getAllFruits()
+            setFruits(allFruits);
         }
-        setFruit('');
+        fetchFruits();
+    }, [])
+
+    const handleAddFruit = (fruit) => {
+        if (!fruit.inStock) {
+            console.log("This fruit is out of stock!")
+            return;
+        }
+        setCollectedFruits([...collectedFruits, fruit])
+
     }
-    console.log(fruitData, "data from search");
-  return (
-    <div>
-        <section>
-            <h2>Search</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="fruit">Enter a fruit:</label>
-                <input 
-                type="text" 
-                name='fruit' 
-                value={fruit}
-                id='fruit'
-                onChange={(e) => setFruit(e.target.value)}
-                />
-            </form>
-        </section>
-    </div>
-  )
+
+    const handleRemoveFruit = (fruit) => {
+        setCollectedFruits(collectedFruits.filter(f => f.id !== fruit.id))
+    }
+
+    const addFruit = (name, emoji) => {
+        const newFruit = {
+            id: fruits.length + 1,
+            name,
+            emoji,
+            color: "gray",
+            inStock: true
+        }
+        setFruits([...fruits, newFruit])
+    }
+
+    return (
+        <>
+            <FruitSearch />
+            <FruitList fruits={fruits} />
+            {/* add Form Fuit */}
+            <FruitForm addFruit={addFruit} />
+            <div>
+                <h1>Fruit Inventory</h1>
+                <div>
+                    <h3>Available Fruits:</h3>
+                    <ul>
+                        {
+                            fruits.map(fruit => (
+                                <li key={fruit.id}>
+                                    <span>{fruit.name} {fruit.emoji}</span>
+                                    {fruit.inStock && (
+                                        <button onClick={() => handleAddFruit(fruit)}>Add to Collection</button>
+                                    )}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <h3>Your Collection: </h3>
+                    <ul>
+                        {
+                            collectedFruits.map(fruit => (
+                                <li key={fruit.id}>
+                                    <span>{fruit.name} {fruit.emoji}</span>
+                                    <button onClick={() => handleRemoveFruit(fruit)}>Remove from Collection</button>
+                                </li>
+                            ))
+                        }
+
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default FruitSearch
+export default Fruits
